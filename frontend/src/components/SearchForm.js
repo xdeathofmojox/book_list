9df332182d7fb357483f7b1404df66d6e898f01a
Will <rbrown127@knights.ucf.edu>
2021-10-24 08:23:34 -0400

    Building out search form

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -1,10 +1,32 @@
-function SearchForm() {
+import { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
 
-  return (
-    <>
-        <h1>Search Form</h1>
-    </>
-  )
+function SearchForm() {
+    const [seriesNameSearchText, setSeriesNameSearchText] = useState("");
+
+    const handleSubmit = (event) => {
+        alert('A name was submitted: ' + seriesNameSearchText);
+        event.preventDefault();
+      };
+
+    const handleChange = (event) => {
+        setSeriesNameSearchText(event.target.value);
+    }
+
+    return (
+        <>
+            <h1>Search Form</h1>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formSeriesName">
+                    <Form.Label>Series Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Series Name" value={seriesNameSearchText.value} onChange={handleChange} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </>
+    )
 }
 
 export default SearchForm;

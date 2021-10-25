07932ed1be76c63bf4a5d4d81e0e6ccb791396bd
Will <rbrown127@knights.ucf.edu>
2021-10-25 16:43:18 -0400

    Start building out Series

--- frontend/src/components/SearchResults.js
+++ frontend/src/components/SearchResults.js
@@ -1,5 +1,6 @@
 import { bookData } from "../data";
 import Book from "./Book"
+import Series from "./Series"
 import { Container, Row } from 'react-bootstrap'
 
 function SearchResults() {
@@ -9,10 +10,11 @@ function SearchResults() {
             <Row className="justify-content-md-left">
                 {bookData.map((data, key) => {
                     return (
-                        <Book key={key} series={data.Name} issue={data.Issue} have={data.Have} need={data.Need}/>
+                        <Book key={data.ID} series={data.Name} issue={data.Issue} have={data.Have} need={data.Need}/>
                     );
                 })}
             </Row>
+            <Series series={"test"} />
         </Container>
         </>
   );
--- frontend/src/components/Series.js
+++ frontend/src/components/Series.js
@@ -1,8 +1,14 @@
 import { Card, Container, Row } from 'react-bootstrap'
 
 function Series(props) {
+
+    const handleClick = (event) => {
+        alert('Clickity');
+        event.preventDefault();
+      };
+
     return (
-        <Card bg="dark" text="white" style={{ width: '18rem' }} className="justify-content-md-center m-3">
+        <Card bg="dark" text="white" style={{ width: '18rem' }} className="justify-content-md-center m-3" onClick={handleClick}>
         <Card.Body>
             <Container>
                 <Row>
